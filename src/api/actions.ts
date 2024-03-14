5c59765a567634bc7a972cd7bb6721c01ef9ace9
rashel8070 <127262814+rashel8070@users.noreply.github.com>
2024-03-14 11:43:24 +0000

    new commit

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://automatic-goldfish-pxrrj45vgx52r5v7-3000.app.github.dev/";
+const API_URL = "https://automatic-goldfish-pxrrj45vgx52r5v7-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
